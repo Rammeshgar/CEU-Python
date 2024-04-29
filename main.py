1d80db8a52b3003093c9c778f5090cb514ab43ea
Sadeq Rezai <123075821+Rammeshgar@users.noreply.github.com>
2024-04-29 21:42:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print('hello world')
+a = 20
+print(a * 30)
+#myname = input('write your name!: ')
+#print('hello my name is ' + myname)
+print('number' + "10")
+print('number' + str(a))
+#how  we make string between num and letter
+print(f'number{a}')
+name = input('what is you name? ')
+myweight = input('what is your weight?(kg) ')
+myweight = int(myweight)
+myheight = input('what is your height? (cm)')
+myheight = int(myheight) / 100
+print(int(myweight) * 2)
+print(type(myweight))
+BMI=myweight/(myheight * myheight)
+print('hi '+name+ f' your BMI num is {BMI} ')
+
